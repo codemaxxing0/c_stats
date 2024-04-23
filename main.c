1259f0a80b5e6766e2be8f87d9176bb98e384605
codemaxxing0 <johncoding3@gmail.com>
2024-04-23 21:48:47 +0200

    reading cells

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
 
-    char filepath[100] = "/home/matt/Desktop/c_pandas/examples/example_01.csv";
+    char filepath[100] = "/home/matt/Desktop/c_stats/examples/example_01.csv";
     
     FILE* contents = fopen(filepath, "r");
 
     rewind(contents);
 
+    typedef struct
+    {
+        int idx;
+        int idy;
+        char value[100];
+    } Cell;
+    
+    int idx = -1;
+    int idy = -1;
+
     char buffer[100];
     while (fgets(buffer, sizeof(buffer), contents) != NULL){
+
+        ++idx;
         printf("%s\n", buffer);
+        char *token = strtok(buffer, ",");
+
+        while(token){
+            ++idy;
+            puts(token);
+            token = strtok(NULL, ",");
+        }
+        
     }
     
 
