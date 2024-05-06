eb4eaf71b15639d89b89bd95ec99082dd6663340
codemaxxing0 <johncoding3@gmail.com>
2024-05-06 23:41:18 +0200

    improvements

--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define N_ROWS 5
+#define N_COLS 3
+#define N_CHARS 100
+
 // DEFINITIONS
-void usage(void);
-void print_table(char table[10][10][100]);
+void print_usage(void);
+void print_table(char table[N_ROWS][N_COLS][N_CHARS]);
 
 
 // MAIN
@@ -26,15 +30,16 @@ int main(int argc, char *argv[]){
         else
         {
             // TODO: make dinamically allocated
-            char buffer[100];
-            char table[10][10][100];
+            char buffer[N_CHARS];
+            char table[N_ROWS][N_COLS][N_CHARS];
 
             int i = 0; //row index
-            int j = 0; //col index
             
             // iterate over all the rows of the file
-            while (fgets (buffer, 100, file_handler) != NULL)
+            while (fgets (buffer, N_CHARS, file_handler) != NULL)
             {   
+                int j = 0; //col index
+                
                 // iterate over all the tokens divided by sep
                 // TODO: make separator a parameter
                 // TODO: handle commas inside the cell
@@ -59,42 +64,28 @@ int main(int argc, char *argv[]){
     }
     else
     {
-        usage();
+        print_usage();
         return 1;
     }
 }
 
 
 // IMPLEMENTATION
-void usage(void){
+void print_usage(void){
     printf("\n1 argument expected\n");
     printf("arg1: provide path to file\n");
     printf("\n");
 }
 
-// TODO: FIX THIS
-void print_table(char table[10][10][100]){
+// TODO: make dinamic
+void print_table(char table[N_ROWS][N_COLS][N_CHARS]){
     int i, j;
 
-    // Print column headers
-    printf("      ");
-    for (j = 0; j < 10; j++) {
-        printf("| %-10d", j);
-    }
-    printf("\n");
+    for (i = 0; i < N_ROWS; i++) {
+        printf("%-5d ", i);
 
-    // Print separator line
-    for (j = 0; j <= 10; j++) {
-        printf("+------------");
-    }
-    printf("+\n");
-
-    // Print array contents
-    for (i = 0; i < 10; i++) {
-        printf("%-5d ", i); // Print row index
-
-        for (j = 0; j < 10; j++) {
-            printf("| %-10s", table[i][j]); // Print array element
+        for (j = 0; j < N_COLS; j++) {
+            printf("| %-10s", table[i][j]);
         }
         printf("\n");
     }
